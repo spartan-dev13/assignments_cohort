fcbb71b011140ce07138bc1b86d07fcadec1b063
Kirandwip Paul <kirandwip.info@gmail.com>
2024-01-30 17:33:15 +0530

    anagram.js done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,19 @@
 */
 
 function isAnagram(str1, str2) {
+  // Check if the lengths are equal
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  // Remove non-alphanumeric characters and convert to lowercase
+  const cleanStr1 = str1.replace(/[^a-zA-Z0-9]/g, '').toUpperCase();
+  const cleanStr2 = str2.replace(/[^a-zA-Z0-9]/g, '').toUpperCase();
 
+  // Sort the characters in both strings and compare
+  const sortedStr1 = cleanStr1.split('').sort().join('');
+  const sortedStr2 = cleanStr2.split('').sort().join('');
+
+  return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
